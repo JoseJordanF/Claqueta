0d4fbc73d3970ecbd961f5688fae14e02c8052a8
JoseJordanF <josejordanfer@gmail.com>
2023-10-16 09:58:39 +0200

    documentacion y PMV del M1

---
+++ ClaquetaTFG/app/src/main/java/com/app/claquetatfg/model/domain/valueObjects/Rating.kt
@@ -0,0 +1,7 @@
+package com.app.claquetatfg.model.domain.valueObjects
+
+data class Rating(
+    val plotRating : Double,
+    val performanceRating : Double,
+    val directionRating : Double,
+){}
