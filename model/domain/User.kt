c0fb8cec09ac7c74819945017efb561ea4f5b753
JoseJordanF <josejordanfer@gmail.com>
2023-10-18 08:30:24 +0200

    Introduccion al capitulo de implementacion y descripcion del PMV del M1

--- model/domain/User.kt
+++
@@ -1,9 +0,0 @@
-package com.app.claquetatfg.model.domain
-
-
-data class User(
-    val username : String,
-    val email: String,
-    val password: String,
-    val reputation : Int = 0
-){}
--- model/domain/valueObjects/Rating.kt
+++
@@ -1,7 +0,0 @@
-package com.app.claquetatfg.model.domain.valueObjects
-
-data class Rating(
-    val plotRating : Double,
-    val performanceRating : Double,
-    val directionRating : Double,
-){}
