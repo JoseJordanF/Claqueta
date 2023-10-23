3229b24f75d5b74857cf4d87074efb70b045ff43
JoseJordanF <josejordanfer@gmail.com>
2023-10-23 11:28:26 +0200

    Los principales atributos que definen a una buena review como se define en la documentacion #44 dado #75

--- domain/Review.kt
+++ domain/Review.kt
@@ -1,5 +1,12 @@
 
+import java.util.*
+
 
 data class Review(
-    
-){}
+    val contentPlot : String,
+    val contentPerformance: String,
+    val contentDirection: String,
+    val containSpoiler: Boolean,
+    val userAuthor : String,
+    val creationDate : Date
+    ){}
