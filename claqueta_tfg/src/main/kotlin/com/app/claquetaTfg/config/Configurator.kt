8d7bb7c1a3d593b11bcaf2e3f7298307e7201183
JoseJordanF <josejordanfer@gmail.com>
2024-04-08 09:34:08 +0200

    Correcciones en el codigo #123
    Demostrando de la manera minima la utilidad de la herramienta

--- claqueta_tfg/src/main/kotlin/com/app/claquetaTfg/config/Configurator.kt
+++ claqueta_tfg/src/main/kotlin/com/app/claquetaTfg/config/Configurator.kt
@@ -16,47 +16,8 @@ object Configurator {
     }
 
     fun loadConfig(pathFile: String) {
-        val separador = if (pathFile.contains('/')) '/' else '\\'
-
-        when {
-            pathFile.endsWith(".env") -> {
-                var directorio = pathFile.substringBeforeLast(separador)
-                var archivo = pathFile.substringAfterLast(separador)
-                val dotenv = Dotenv
-                    .configure()
-                    .directory(directorio)
-                    .filename(archivo)
-                    .load()
-                val envMap = dotenv.entries().associateBy({ it.key }, { it.value })
-                val properties = Properties()
-                properties.putAll(envMap)
-                config = config.withFallback(ConfigFactory.parseProperties(properties))
-            }
-            pathFile.endsWith(".json") -> {
-                val jsonConfig: Config = ConfigFactory.parseFile(File(pathFile))
-                config = config.withFallback(jsonConfig)
-            }
-            pathFile.endsWith(".properties") -> {
-                val properties = Properties()
-                FileInputStream(pathFile).use { input ->
-                    properties.load(input)
-                }
-                val propertiesConfig: Config = ConfigFactory.parseProperties(properties)
-                config = config.withFallback(propertiesConfig)
-            }
-            pathFile.endsWith(".conf") -> {
-                val hoconConfig: Config = ConfigFactory.parseFile(File(pathFile))
-                config = config.withFallback(hoconConfig)
-            }
-            else -> {
-                try {
-                    val textStringConfig: Config = ConfigFactory.parseString(pathFile)
-                    config = config.withFallback(textStringConfig)
-                }catch (e: Exception){
-                    println("Error: extensión de archivo no reconocida: ${e.message}")
-                }
-            }
-        }
+        val fileConfig: Config = ConfigFactory.parseFile(File(pathFile))
+        config = config.withFallback(fileConfig)
     }
 
     fun getConfig(key: String) = config.getString(key)
--- claqueta_tfg/src/test/kotlin/com/app/claquetaTfg/config/ConfiguratorTest.kt
+++ claqueta_tfg/src/test/kotlin/com/app/claquetaTfg/config/ConfiguratorTest.kt
@@ -2,8 +2,6 @@ package com.app.claquetaTfg.config
 
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
-import java.lang.RuntimeException
 import kotlin.test.assertFalse
 import kotlin.test.assertNotNull
 
@@ -15,25 +13,10 @@ class ConfiguratorTest{
     }
 
     @Test
-    fun `When I want to load the configuration of an env file`() {
-
-        val envFilePath =
-            "src/test/resources/configuracion.env"
-
-        //When
-        Configurator.loadConfig(envFilePath)
-        println(Configurator.getConfig("DB_PORT"))
-        val res = Configurator.getConfig("DB_PORT")
-
-        //Then
-        assertNotNull(res)
-    }
-
-    @Test
     fun `When I want to load the configuration of a HOCON file`() {
 
         val hoconFilePath =
-            "src/test/resources/configuracion.conf"
+            "C:\\Users\\34625\\Desktop\\JOB\\TFG\\DEFINITIVO\\claqueta_tfg\\src\\test\\resources\\configuracion.conf"
 
         //When
         Configurator.loadConfig(hoconFilePath)
@@ -48,7 +31,7 @@ class ConfiguratorTest{
     fun `When I want to load the configuration of a properties file`() {
 
         val propertiesFilePath =
-            "src/test/resources/configuracion.properties"
+            "C:\\Users\\34625\\Desktop\\JOB\\TFG\\DEFINITIVO\\claqueta_tfg\\src\\test\\resources\\configuracion.properties"
 
         //When
         Configurator.loadConfig(propertiesFilePath)
@@ -63,7 +46,7 @@ class ConfiguratorTest{
     fun `When I want to load the configuration of a JSON file`() {
 
         val josnFilePath =
-            "src/test/resources/Configuracion.json"
+            "C:\\Users\\34625\\Desktop\\JOB\\TFG\\DEFINITIVO\\claqueta_tfg\\src\\test\\resources\\configuracion.json"
 
         //When
         Configurator.loadConfig(josnFilePath)
@@ -75,89 +58,28 @@ class ConfiguratorTest{
     }
 
     @Test
-    fun `When I want to load the configuration from inline text strings`() {
-
-        val configString = """
-        database {
-          host = "localhost"
-          port = 5432
-          credentials {
-            username = "myuser"
-            password = "mypassword"
-          }
-          options {
-            timeout = 30
-            max_connections = 100
-          }
-        }
-    """.trimIndent()
-
-        //When
-        Configurator.loadConfig(configString)
-        println(Configurator.getConfig("database.options.max_connections"))
-        val res = Configurator.getConfig("database.options.max_connections")
-
-        //Then
-        assertNotNull(res)
-    }
-
-    @Test
-    fun `When I want to load the string configuration inline, but it is not an acceptable string`() {
-
-        //When
-        val configString = "String no aceptable para ser procesado".trimIndent()
-
-        //Then
-        assertThrows<RuntimeException> {
-            Configurator.loadConfig(configString)
-            println(Configurator.getConfig("database.options.max_connections"))
-        }
-    }
-
-    @Test
     fun `When I want to load all settings`() {
 
-        val envFilePath =
-            "src/test/resources/configuracion.env"
         val hoconFilePath =
-            "src/test/resources/configuracion.conf"
+            "C:\\Users\\34625\\Desktop\\JOB\\TFG\\DEFINITIVO\\claqueta_tfg\\src\\test\\resources\\configuracion.conf"
         val propertiesFilePath =
-            "src/test/resources/configuracion.properties"
+            "C:\\Users\\34625\\Desktop\\JOB\\TFG\\DEFINITIVO\\claqueta_tfg\\src\\test\\resources\\configuracion.properties"
         val josnFilePath =
-            "src/test/resources/configuracion.json"
-        val configString = """
-        database {
-          host = "localhost"
-          port = 5432
-          credentials {
-            username = "myuser"
-            password = "mypassword"
-          }
-          options {
-            timeout = 30
-            max_connections = 100
-          }
-        }
-        """.trimIndent()
+            "C:\\Users\\34625\\Desktop\\JOB\\TFG\\DEFINITIVO\\claqueta_tfg\\src\\test\\resources\\configuracion.json"
+
 
         //When
-        Configurator.loadConfig(envFilePath)
         Configurator.loadConfig(hoconFilePath)
         Configurator.loadConfig(propertiesFilePath)
         Configurator.loadConfig(josnFilePath)
-        Configurator.loadConfig(configString)
 
         val configList = listOf<String>(
-            Configurator.getConfig("DB_PORT"), Configurator.getConfig("database.options.timeout"),
-            Configurator.getConfig("url.database"), Configurator.getConfig("database.credentials.username"),
-            Configurator.getConfig("database.options.max_connections")
+            Configurator.getConfig("database.options.timeout"),
+            Configurator.getConfig("url.database"), Configurator.getConfig("database.credentials.password"),
         )
-
         println(configList[0])
         println(configList[1])
         println(configList[2])
-        println(configList[3])
-        println(configList[4])
 
         //Then
         assertFalse { configList.any { it.isEmpty() } }
