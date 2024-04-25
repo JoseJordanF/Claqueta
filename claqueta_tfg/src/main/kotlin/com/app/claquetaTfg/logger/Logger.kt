88a5009219fa5dd0d6103ee9a13df7f7b6efa40c
JoseJordanF <josejordanfer@gmail.com>
2024-04-25 14:32:25 +0200

    Correcciones en el codigo del Logger
    Se corrigen errores que no hacian singleton a Logger
    
    Tambien se añade una estructura de datos para poder obtener los logs
    
    ademas se decide poner los test sobre logs en otro fichero de test ya que tiene mas sentido hacerlos donde se realicen las actividades que hay que observar

--- claqueta_tfg/src/main/kotlin/com/app/claquetaTfg/logger/Logger.kt
+++ claqueta_tfg/src/main/kotlin/com/app/claquetaTfg/logger/Logger.kt
@@ -4,40 +4,55 @@ import com.app.claquetaTfg.config.Configurator
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-class Logger {
-     lateinit var logger: Logger
-
+class Logger private constructor(){
+    private lateinit var logger: Logger
+    private lateinit var logInMemory: MutableList<Pair<String, String>>
 
     companion object {
-        private lateinit var instance: com.app.claquetaTfg.logs.Logger
+        @Volatile private var instance: com.app.claquetaTfg.logs.Logger? = null
 
-        fun instance(clazz: Class<*> = this::class.java): com.app.claquetaTfg.logs.Logger {
-            val configFile = Configurator.instance().getConfig("CLAQUETA_LOGBACK_CONFIG")
+        fun instance(
+            classType: Class<*> = this::class.java,
+            configName: String
+        ): com.app.claquetaTfg.logs.Logger {
+            val configFile = Configurator.instance().getConfig(configName)
             System.setProperty("logback.configurationFile", configFile!!)
-            instance = Logger()
-            instance.logger = LoggerFactory.getLogger(clazz)
+            instance ?: synchronized(this) {
+                instance ?: Logger().also { instance = it }
+            }
+            instance!!.logger = LoggerFactory.getLogger(classType)
+            instance!!.logInMemory = mutableListOf()
 
-            return instance
+            return instance as com.app.claquetaTfg.logs.Logger
         }
     }
 
     fun trace(msg: String) {
         logger.trace(msg)
+        this.logInMemory.add(Pair("trace",msg))
     }
 
     fun debug(msg: String) {
         logger.debug(msg)
+        this.logInMemory.add(Pair("debug",msg))
     }
 
     fun info(msg: String) {
         logger.info(msg)
+        this.logInMemory.add(Pair("info",msg))
     }
 
     fun warn(msg: String) {
         logger.warn(msg)
+        this.logInMemory.add(Pair("warn",msg))
     }
 
     fun error(msg: String) {
         logger.error(msg)
+        this.logInMemory.add(Pair("error",msg))
+    }
+
+    fun getLogsInMemory():MutableList<Pair<String, String>>{
+        return this.logInMemory
     }
 }
