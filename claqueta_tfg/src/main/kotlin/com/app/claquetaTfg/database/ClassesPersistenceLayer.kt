9e856dcfaaff797ef0fbff1240c93c1cefbb9282
JoseJordanF <josejordanfer@gmail.com>
2024-06-04 18:05:37 +0200

    Se añaden implementaciones para las recomendaciones y usuarios #147
    No habia implementaciones que nos permitieran guardar y cargar estos datos si fuera necesario
    
    Ambos otorgan valor por una parte las recomendaciones #36 y por otra parte al desarrollador #136

--- claqueta_tfg/src/main/kotlin/com/app/claquetaTfg/database/ClassesPersistenceLayer.kt
+++ claqueta_tfg/src/main/kotlin/com/app/claquetaTfg/database/ClassesPersistenceLayer.kt
@@ -4,7 +4,14 @@ import com.app.claquetaTfg.domain.Film
 import com.app.claquetaTfg.domain.Review
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.buildJsonArray
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
+import kotlinx.serialization.json.add
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonObject
 import java.io.File
 import kotlin.reflect.KClass
 import kotlin.reflect.cast
@@ -79,6 +86,83 @@ class DatabasePersistenceLayer<T : Any>(
     }
 }
 
+class RecomendationsDatabasePersistenceLayer(
+    fileJson: String,
+) : PersistenceLayer {
+    private val jsonPrettyFormat = Json { prettyPrint = true }
+    private val file = File(fileJson)
+
+    override fun saveData(data: Any) {
+        if (!file.exists()) {
+            file.createNewFile()
+        }
+        val map = data as HashMap<String, List<String>>
+
+        val list = buildJsonArray {
+            map.forEach { (first, second) ->
+                add(
+                    buildJsonObject {
+                        put("first", first)
+                        put("second", buildJsonArray { second.forEach { add(it) } })
+                    }
+                )
+            }
+        }
+
+        val json = jsonPrettyFormat.encodeToString(list)
+        file.writeText(json)
+    }
+
+    override fun loadData(): HashMap<String, List<String>> {
+        val content = file.readText()
+        if (content.isEmpty()) return hashMapOf()
+        val list = jsonPrettyFormat.parseToJsonElement(content).jsonArray
+        val map = hashMapOf<String, List<String>>()
+        list.forEach {
+            val jobject = it.jsonObject
+            val first = jobject["first"].toString().trim('"')
+            val second = jobject["second"]?.jsonArray?.map { it.toString().trim('"') }?.toList()
+            if (second != null) {
+                map[first] = second
+            }
+        }
+        return map
+    }
+}
+
+class UsersDatabasePersistenceLayer(
+    fileJson: String,
+) : PersistenceLayer {
+    private val jsonPrettyFormat = Json { prettyPrint = true }
+    private val file = File(fileJson)
+
+    override fun saveData(data: Any) {
+        if (!file.exists()) {
+            file.createNewFile()
+        }
+        val json = jsonPrettyFormat.encodeToString(data as String)
+        val isNewFile = file.length() == 0L
+        if (isNewFile) {
+            file.appendText("[\n$json\n]")
+        } else {
+            val content = file.readText()
+            val contentWithoutClosingBracket = content.removeSuffix("]")
+            file.writeText("$contentWithoutClosingBracket,$json\n]")
+        }
+    }
+
+    override fun loadData(): HashSet<String> {
+        val content = file.readText()
+        if (content.isEmpty()) return hashSetOf()
+        val list = jsonPrettyFormat.parseToJsonElement(content).jsonArray
+        val userList = hashSetOf<String>()
+        list.forEach {
+            val jobject = it.toString()
+            userList.add(jobject.trim('"'))
+        }
+        return userList
+    }
+}
 
 class DataManager(private val persistenceLayer: PersistenceLayer) {
     fun saveData(data: Any) {
